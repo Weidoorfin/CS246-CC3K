73d0c53a7bd3bc2bc9cbcf18c7b2974dc7d5ea96
d8wei <d8wei@uwaterloo.ca>
2025-07-20 15:39:29 -0400

    putting in ramdomgenerator

--- main.cc
+++ main.cc
@@ -1,13 +1,21 @@
 // main.cc
 // This file is part of CC3K, a C++ roguelike game.
 
+import <fstream>;
 import game;
 import display;
 
 using namespace std;
 
-int main() {
-    Game g;
+int main(int argc, char* argv[]) {
+    // Initialize the game
+    if (argc > 1) {
+        ifstream inFile{argv[1]};
+        Game g(inFile);
+    }
+    else {
+        Game g();
+    }
     g.setDisplay();
     g.init();
     return 0;
