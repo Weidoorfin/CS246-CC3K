5eb8406c20d29620973d51a2d2b8f924d7563053
Jenny Chen <chenjunyinini@gmail.com>
2025-07-20 23:43:48 -0400

    treasure

--- Treasure.cc
+++ Treasure.cc
@@ -3,8 +3,31 @@ export module treasure;
 import item;
 
 export class Treasure : public Item {
+protected:
     int value; // Amount of gold this treasure is worth
 public:
     Treasure(int value);
     int getValue() const;
+    std::unique_ptr<Player> applyEffect(std::unique_ptr<Player> player) override;
+    virtual ~Treasure() = default;
+};
+
+export class SmallPile : public Treasure {
+public:
+    SmallPile();
+};
+
+export class NormalPile : public Treasure {
+public:
+    NormalPile();
+};
+
+export class MerchantHoard : public Treasure {
+public:
+    MerchantHoard();
+};
+
+export class DragonHoard : public Treasure {
+public:
+    DragonHoard();
 };
