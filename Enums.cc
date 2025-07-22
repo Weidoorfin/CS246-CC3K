50cddb58b442eac88cd099bab01a8d4753ecc5de
Doorfin <weidoorfin@gmail.com>
2025-07-21 21:53:43 -0400

    Worked a lot on inputing files

--- Enums.cc
+++ Enums.cc
@@ -5,4 +5,4 @@ enum class Direction {N, NE, E, SE, S, SW, W, NW};
 enum class PotionType {BA, BD, PH, WA, WD, RH};
 enum class GameState {Finish, Restart, Quit};
 enum class EntityType {UNINITIALIZED, PLAYER, ENEMY, POTION, TREASURE};
-enum class TileType {VerticalWall, HorizontalWall, Floor, Stair, Door, Entity, Nothing, Corridor};
+enum class TileType {VerticalWall, HorizontalWall, Floor, Stair, Door, Player, Enemy, Potion, Gold, Nothing, Corridor};
